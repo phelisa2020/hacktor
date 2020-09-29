601aeb31ee0e9b605657db2a2c0ece5f4de39d8b
phelisa2020 <phelisagqwetha@gmail.com>
2020-09-29 12:27:14 +0200

    first commits

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+
+
+
+app.engine('handlebars', exphbs({
+	layoutsDir: './views/layouts'
+
+
+}));
+
+
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false })); // add this line
+app.use(bodyParser.json()); // add  this line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3025;
+app.listen(PORT, function(){
+	console.log('App started at port:', PORT);
+})
